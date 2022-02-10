b9abe5f2b44d6d24b850dd927ab16d93b4a5b57b
matt0798 <matthew.weakley@g.austincc.edu>
2022-02-10 17:32:54 -0600

    hw 2

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -1,4 +1,4 @@
-#include "expressions.h"
+#include "../src/homework/02_expressions/expressions.h"
 
 //example
 int add_numbers(int num1, int num2)
@@ -9,6 +9,11 @@ int add_numbers(int num1, int num2)
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
+const double tax_rate = .0675;
+double get_sales_tax_amount(double meal_amount){
+	return tax_rate * meal_amount;
+}
 
-
-
+double get_tip_amount(double meal_amount, double tip_rate){
+	return meal_amount * tip_rate;
+}
--- test/homework_test/02_expressions_test/02_expressions_tests.cpp
+++ test/homework_test/02_expressions_test/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "variables.h"
+#include "../src/homework/02_expressions/expressions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -12,3 +13,13 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Test get sales tax."){
+	REQUIRE(get_sales_tax_amount(10.0) == .675);
+	REQUIRE(get_sales_tax_amount(20.0) == 1.35);
+
+}
+
+TEST_CASE("Test get tip amount."){
+	REQUIRE(get_tip_amount(20.0, .15) == 3);
+	REQUIRE(get_tip_amount(20.0, .20) == 4);
+}
